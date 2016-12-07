8c67cc3406f26a742760869b074bc9fe5ed0614a
power <powerfuldeff@gmail.com>
2016-12-07 23:39:06 +0300

    Server list

--- Assets/MyScripts/NetworkMenager.cs
+++ Assets/MyScripts/NetworkMenager.cs
@@ -9,15 +9,14 @@ public class NetworkMenager : MonoBehaviour {
 
 	public void StartServer()
 	{
-		if (!Network.isServer && !Network.isClient) {
-			Network.InitializeServer (4, 25000, !Network.HavePublicAddress ());
-			MasterServer.RegisterHost (typeName, gameName);
-		} else {
-			Network.Disconnect ();
-			MasterServer.UnregisterHost ();
-			Debug.LogWarning ("Server stopped.");
-			//Debug.LogWarning ("Server already created.");
-		}
+		Network.InitializeServer (4, 25000, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost (typeName, gameName);
+	}
+
+	public void StopServer() {
+		Network.Disconnect ();
+		MasterServer.UnregisterHost ();
+		Debug.LogWarning ("Server stopped.");
 	}
 
 	void OnServerInitialized()
@@ -25,8 +24,55 @@ public class NetworkMenager : MonoBehaviour {
 		Debug.Log("Server Initializied");
 	}
 
+	void OnGUI(){
+		if (!Network.isClient && !Network.isServer) {
+			if (GUI.Button (new Rect (Screen.width/2-75, 50, 150, 100), "Start Server"))
+				StartServer ();
+		} else {
+			if (GUI.Button (new Rect (Screen.width/2-75, 50, 150, 100), "Stop Server"))
+				StopServer ();
+		}
+
+		if (GUI.Button(new Rect(Screen.width/2-75, 200, 150, 100), "Refresh Hosts"))
+			RefreshHostList();
+
+		if (hostList != null)
+		{
+			for (int i = 0; i < hostList.Length; i++)
+			{
+				if (GUI.Button(new Rect(30, 50 + (110 * i), 150, 100), hostList[i].gameName))
+					JoinServer(hostList[i]);
+			}
+		}
+	}
+
 	public void writeLine(string a){
 		Debug.Log (a);
+	}
+
+	//!!!
+
+	private HostData[] hostList;
+
+	private void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+
+	void OnConnectedToServer()
+	{
+		Debug.Log("Server Joined");
 	}
 
 }
