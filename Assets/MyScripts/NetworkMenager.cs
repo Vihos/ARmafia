48eecd23451e99c52c4c2aa1e770a7384d06a036
power <powerfuldeff@gmail.com>
2016-12-07 23:00:21 +0300

    Network Menager

---
+++ Assets/MyScripts/NetworkMenager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkMenager : MonoBehaviour {
+	
+	private const string typeName = "ARMafia";
+	private const string gameName = "testRoom";
+
+	public void StartServer()
+	{
+		if (!Network.isServer && !Network.isClient) {
+			Network.InitializeServer (4, 25000, !Network.HavePublicAddress ());
+			MasterServer.RegisterHost (typeName, gameName);
+		} else {
+			Network.Disconnect ();
+			MasterServer.UnregisterHost ();
+			Debug.LogWarning ("Server stopped.");
+			//Debug.LogWarning ("Server already created.");
+		}
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+	}
+
+	public void writeLine(string a){
+		Debug.Log (a);
+	}
+
+}
