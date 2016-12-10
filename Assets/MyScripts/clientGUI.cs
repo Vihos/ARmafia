47b55f3e3c049427e52f5af88015db695ee8a4c1
power <powerfuldeff@gmail.com>
2016-12-10 16:44:25 +0300

    Multiplayer Alpha v2

--- Assets/MyScripts/clientGUI.cs
+++ Assets/MyScripts/clientGUI.cs
@@ -29,22 +29,11 @@ public class clientGUI : MonoBehaviour {
 			for (int i = 0; i < hostList.Length; i++)
 			{
                 // TODO Change positions
-                if (GUI.Button (new Rect (30, Screen.height / 4 + 20 + ((Screen.height / 8 + 10) * i), Screen.width / 3, Screen.height / 8), hostList[i].gameName)) { 
-					JoinServer(hostList[i]);
+                if (GUI.Button (new Rect (30, Screen.height / 4 + 20 + ((Screen.height / 8 + 10) * i), Screen.width / 3, Screen.height / 8), hostList[i].gameName)) {
+                    ClientMenager.JoinServer(hostList[i]);
 				}
 			}
 		}
 	}
-
-    private void JoinServer(HostData hostData)
-    {
-        Network.Connect(hostData);
-    }
-
-    // Debug on server connect Event
-    void OnConnectedToServer()
-    {
-        Debug.Log("Server Joined");
-    }
 
 }
--- Assets/MyScripts/serverGUI.cs
+++ Assets/MyScripts/serverGUI.cs
@@ -6,12 +6,22 @@ public class serverGUI : MonoBehaviour {
 
 	private const string typeName = "ARMafia";
 	private string gameName = "Your Room";
+    private NetworkPlayer[] netPlayers;
 
-	void OnGUI(){
+    void OnGUI(){
 		if (!Network.isClient && !Network.isServer) {
-			gameName = GUI.TextField (new Rect(2*Screen.width/3,0,Screen.width/3,Screen.height/8), gameName, 25);
+			gameName = GUI.TextField (new Rect(2 * Screen.width / 3, 0, Screen.width / 3, Screen.height / 8), gameName, 25);
 
-			if (GUI.Button (new Rect (30, 0, Screen.width/3, Screen.height/8), "Start Server"))
+        if(Network.isServer)
+            {
+                netPlayers = Network.connections;
+                for (int i = 0; i < netPlayers.Length; i++)
+                {
+                    Debug.Log(netPlayers[i]);
+                }
+            }
+
+            if (GUI.Button (new Rect (30, 0, Screen.width/3, Screen.height/8), "Start Server"))
 				NetworkMenager.StartServer (typeName, gameName);
 		} else {
 			if (GUI.Button (new Rect (30, 0, Screen.width/3, Screen.height/8), "Stop Server"))
