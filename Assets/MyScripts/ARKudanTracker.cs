0dd76fd3d4ec6a6e8f294146a6e55c997dce89ba
jokermanx <jokermanx1@rambler.ru>
2016-12-15 21:31:17 +0300

    temp

--- Assets/MyScripts/ARKudanTracker.cs
+++ Assets/MyScripts/ARKudanTracker.cs
@@ -8,18 +8,28 @@ using Kudan.AR;
 
 	public class ARKudanTracker : MonoBehaviour
 	{
-		public ARKudanTracker kudanTracker;
-		protected Trackable trac;
+		public static ARKudanTracker kudanTracker;
+		protected Trackable[] trac;
 		public TrackerBase _trackerPlugin;
 		public void Start(){
-			//_trackerPlugin = kudanTracker._trackerPlugin;
 		} 
 			public void DebugLog()
 			{
-				//kudanTracker._lastDetectedTrackables;
-		///trac = _trackerPlugin.GetTrackable;
-				//Debug.Log(_lastDetectedTrackables[0]);
+		Debug.Log(kudanTracker._trackerPlugin.GetNumTrackables());
 			}
-
+		void GetPlugin ()
+		{
+			#if UNITY_EDITOR_OSX
+			_trackerPlugin = new TrackerOSX();
+			checkEditorLicenseKey();
+			checkLicenseKeyValidity();
+			#elif UNITY_EDITOR_WIN
+			_trackerPlugin = new TrackerWindows();	
+			#elif UNITY_IOS
+			_trackerPlugin = new TrackeriOS(_background);
+			#elif UNITY_ANDROID
+			_trackerPlugin = new TrackerAndroid(_background);
+			#endif 
+		}
 	}
 
