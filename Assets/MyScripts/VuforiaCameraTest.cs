515651ce58f0414f97ac786cbfac14ec456a73f9
power <powerfuldeff@gmail.com>
2016-12-21 20:22:47 +0300

    Vuforia AR Positions

---
+++ Assets/MyScripts/VuforiaCameraTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VuforiaCameraTest : MonoBehaviour {
+
+    public GameObject cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(this.transform.rotation.z);
+        //Debug.Log(this.transform.position);
+        //z
+        cube.transform.position = new Vector3(100,0,190);
+        cube.transform.rotation = new Quaternion(45f,45f,45f,1f);
+    }
+}
---
+++ Assets/MyScripts/VuforiaTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VuforiaTest : MonoBehaviour, Vuforia.ITrackableEventHandler
+{
+    private TrackableBehaviour myTrackableBehaviour;
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnTrackingFound()
+    {
+        Debug.Log("a");
+    }
+
+    void Start () {
+        myTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (myTrackableBehaviour)
+        {
+            myTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+	
+	void Update () {
+        //Debug.Log(myTrackableBehaviour.transform.position);
+    }
+}
