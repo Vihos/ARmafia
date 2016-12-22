d47a58ccdb2b0e20d2c48c96f3b659d6fc7cf2bd
power <powerfuldeff@gmail.com>
2016-12-22 18:22:24 +0300

    Cards Scene

--- Assets/MyScripts/VuforiaCameraTest.cs
+++ Assets/MyScripts/VuforiaCameraTest.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VuforiaCameraTest : MonoBehaviour {
 
-    public GameObject cube;
+    public GameObject card;
+    public Text markerPosText;
+    public Text cameraPosText;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +16,21 @@ public class VuforiaCameraTest : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(this.transform.rotation.z);
+
+
+        //card.transform.position = new Vector3(card.transform.position.x, 100f, card.transform.position.z);
+        //card.transform.rotation = new Quaternion (card.transform.rotation.x, card.transform.rotation.y, this.transform.rotation.z, this.transform.rotation.w);
+
+        //markerPosText.text = "Marker rotation x = " + this.transform.rotation.x.ToString() + " y =  "+ this.transform.rotation.y.ToString() + " z = " + this.transform.rotation.z.ToString();
+        //cameraPosText.text = "Camera position: " + this.transform.position.ToString();
+
+        //card.transform.position = new Vector3(0f,100f,0f);
+
+
+        //Debug.Log(this.transform.rotation);
         //Debug.Log(this.transform.position);
         //z
-        cube.transform.position = new Vector3(100,0,190);
-        cube.transform.rotation = new Quaternion(45f,45f,45f,1f);
+        //cube.transform.position = new Vector3(100,0,190);
+        //cube.transform.rotation = new Quaternion(45f,45f,45f,1f);
     }
 }
