49bd7512cf583a8f8108e467d6c54b6db537fad2
power <powerfuldeff@gmail.com>
2016-12-23 11:02:46 +0300

    NetView change

--- Assets/MyScripts/CreateServer.cs
+++ Assets/MyScripts/CreateServer.cs
@@ -7,7 +7,7 @@ public class CreateServer : MonoBehaviour {
 
     public InputField RoomName;
     public Dropdown PlayerCount;
-    public NetworkView networkview;
+    private NetworkView networkview;
 
     public Canvas ModalWindow;
     public Text ModalTitle;
@@ -19,6 +19,8 @@ public class CreateServer : MonoBehaviour {
 
     public void Start()
     {
+        networkview = GameObject.Find("NetworkView").GetComponent<NetworkView>();
+
         if (Network.isServer)
         {
             CreateModalWindow("Внимание!", "Сервер уже запущен на вашем устройстве.");
