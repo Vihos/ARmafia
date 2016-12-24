68ffb17d6d6db0c326c59556ad1e950715e19ca0
jokermanx <jokermanx1@rambler.ru>
2016-12-25 01:30:18 +0300

    timer

---
+++ Assets/MyScripts/Gui/DayNight.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+using UnityEngine.SceneManagement;
+
+public class DayNight : MonoBehaviour {
+	public Text time;
+	public Image moonSoon;
+	private bool isDay = true;
+	private int count=10;
+	private int max=20;
+	// Use this for initialization
+	void Start () {
+		
+		//moonSoon.transform.rotation= new Quaternion(0f,0f,180f,0f);
+		Invoke("UpdateTimer", 1);
+	}
+	void UpdateTimer( ){
+
+		if (count > 0) {
+			if (isDay) {
+				time.text = (count / max).ToString () + ":" + (count % max).ToString ();
+				moonSoon.transform.rotation= Quaternion.Euler(new Vector3(0, 0, 180*count/max));
+				Invoke ("UpdateTimer", 1);
+				count--;
+			} else {
+				time.text = (count / max).ToString () + ":" + (count % max).ToString ();
+				moonSoon.transform.rotation= Quaternion.Euler(new Vector3(0, 0, 180+180*count/max));
+				Invoke ("UpdateTimer", 1);
+				count--;
+			}
+		} else {
+			isDay = !(isDay);
+			count = max;
+			Invoke ("UpdateTimer", 0);
+		}
+	} 
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
