066095ec5559a09626c81132b81017dfddd13150
jokermanx <jokermanx1@rambler.ru>
2016-12-24 23:04:32 +0300

    Changing server and client
    Two differents files

---
+++ Assets/MyScripts/Gui/StartServerGui.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class StartServerGui : MonoBehaviour {
+public InputField RoomName;
+	public Dropdown PlayerCount;
+	private NetworkView networkview;
+
+	public Canvas ModalWindow;
+	public Text ModalTitle;
+	public Text ModalReport;
+	private string CurrentRoomName = "";
+	private const string typeName = "ARMafia";
+
+	private NetworkServerLib networkservlib;
+
+	public void Start()
+	{
+		networkservlib  = GameObject.Find("NetworkV").GetComponent<NetworkServerLib>();
+		networkservlib.findNetworkGui ();
+		Debug.Log ("found network server");
+		if (networkservlib.IsServer())
+		{
+			CreateModalWindow("Внимание!", "Сервер уже запущен на вашем устройстве.");
+		}
+	}
+
+	public void ChangeRoomName()
+	{
+		CurrentRoomName = RoomName.text;
+		networkservlib.ChangeRoomName (RoomName.text);
+	}
+
+	public void ChangePlayerCount()
+	{
+		networkservlib.ChangePlayerCount (PlayerCount.value);
+		Debug.Log(PlayerCount.value+3);
+	}
+
+	public void CreateModalWindow(string title,string error)
+	{
+		ModalTitle.text = title;
+		ModalReport.text = error;
+		ModalWindow.gameObject.SetActive(true);
+		Debug.Log("Started modal with text: " + error);
+	}
+
+	public void RemoveModalWindow()
+	{
+		ModalWindow.gameObject.SetActive(false);
+		Debug.Log("Removed Modal Window");
+	}
+
+	public void HostServer()
+	{
+		if (CurrentRoomName.Length > 2)
+		{
+			networkservlib.HostServer ();
+		}
+		else if (CurrentRoomName.Length < 3)
+		{
+			CreateModalWindow("Ошибка!", "Имя сервера должно иметь длину не менне 3 символов.");
+		}
+	}
+
+}
---
+++ Assets/MyScripts/Network/NetworkServerLib.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class NetworkServerLib : MonoBehaviour {
+
+
+	public NetworkView networkview;
+	private string CurrentRoomName = "";
+	private int CurrentPlayerCount = 3;
+	private const string typeName = "ARMafia";
+	private StartServerGui startServerGui;
+	public void Start()
+	{
+		
+	}
+	public void findNetworkGui(){
+		startServerGui  = GameObject.Find("NetworkV").GetComponent<StartServerGui>();
+	}
+	public bool IsServer(){
+		return Network.isServer;
+	}
+	public void ChangePlayerCount(int value)
+	{
+		CurrentPlayerCount = value+3;
+		Debug.Log(value+3);
+	}
+
+	public void ChangeRoomName(string text)
+	{
+		CurrentRoomName = text;
+	}
+
+	public void HostServer()
+	{
+		Network.InitializeServer(CurrentPlayerCount, 25000, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, CurrentRoomName);
+		Debug.Log("Server Started!");
+	}
+
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+	}
+
+	void OnConnectedToServer()
+	{
+		networkview = GameObject.Find("ClientScripts").GetComponent<NetworkView>();
+		networkview.RPC("Change",RPCMode.AllBuffered,"Vuforia");
+	}
+
+}
