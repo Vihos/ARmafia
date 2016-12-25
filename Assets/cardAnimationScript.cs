e69c8571ece4e554e0f590feef349e2067c705ea
power <powerfuldeff@gmail.com>
2016-12-25 13:08:08 +0300

    commit

---
+++ Assets/cardAnimationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardAnimationScript : MonoBehaviour {
+
+    Animator anim;
+    int show = Animator.StringToHash("Base Layer.show");
+    int hide = Animator.StringToHash("hide");
+
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	void Update () {
+        anim.SetTrigger(show);
+    }
+}
