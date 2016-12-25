7f383f77fb0e00eabafd64a1397bf96a077d875a
power <powerfuldeff@gmail.com>
2016-12-25 19:09:27 +0300

    merge fix

--- Assets/MyScripts/Gui/DayNight.cs
+++ Assets/MyScripts/Gui/DayNight.cs
@@ -4,55 +4,62 @@ using UnityEngine;
 using UnityEngine.UI; // Required when Using UI elements.
 using UnityEngine.SceneManagement;
 
-public class DayNight : MonoBehaviour {
-	public Text time;
-	public Image moonSoon;
-	private bool isDay = true;
-<<<<<<< HEAD
-	private int count=180;
-	private int max= 180;
-=======
-	private int timer=10;
-	private int max=20;
-	private NetworkCLientLib networklib;
->>>>>>> refs/remotes/origin/dev.lasthope
-	// Use this for initialization
-	void Start () {
-		networklib  = GameObject.Find("NetworkV").GetComponent<NetworkCLientLib>();
-		networklib.FindDayNight();
-		Invoke("UpdateTimer", 1);
-	}
-	void UpdateTimer( ){
+public class DayNight : MonoBehaviour
+{
+    public Text time;
+    public Image moonSoon;
+    private bool isDay = true;
+    private int timer = 10;
+    private int max = 20;
+    private NetworkCLientLib networklib;
+    // Use this for initialization
+    void Start()
+    {
+        networklib = GameObject.Find("NetworkV").GetComponent<NetworkCLientLib>();
+        networklib.FindDayNight();
+        Invoke("UpdateTimer", 1);
+    }
+    void UpdateTimer()
+    {
 
-		if (timer > 0) {
-			if (isDay) {
-				time.text = (timer / 60).ToString () + ":" + (timer % 60).ToString ();
-				moonSoon.transform.rotation= Quaternion.Euler(new Vector3(0, 0, 180+180*timer/max));
-				Invoke ("UpdateTimer", 1);
-				timer--;
-			} else {
-				time.text = (timer / 60).ToString () + ":" + (timer % 60).ToString ();
-				moonSoon.transform.rotation= Quaternion.Euler(new Vector3(0, 0, 180*timer/max));
-				Invoke ("UpdateTimer", 1);
-				timer--;
-			}
-		} else {
-			isDay = !(isDay);
-			timer = max;
-			Invoke ("UpdateTimer", 0);
-		}
-	} 
-	public void SetDay(){
-		isDay = true;
-		timer = max;
-	}
-	public void SetNight(){
-		isDay = false;
-		timer = max;
-	}
+        if (timer > 0)
+        {
+            if (isDay)
+            {
+                time.text = (timer / 60).ToString() + ":" + (timer % 60).ToString();
+                moonSoon.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180 + 180timer / max));
+                Invoke("UpdateTimer", 1);
+                timer--;
+            }
+            else
+            {
+                time.text = (timer / 60).ToString() + ":" + (timer % 60).ToString();
+                moonSoon.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180timer / max));
+                Invoke("UpdateTimer", 1);
+                timer--;
+            }
+        }
+        else
+        {
+            isDay = !(isDay);
+            timer = max;
+            Invoke("UpdateTimer", 0);
+        }
+    }
+    public void SetDay()
+    {
+        isDay = true;
+        timer = max;
+    }
+    public void SetNight()
+    {
+        isDay = false;
+        timer = max;
+    }
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
